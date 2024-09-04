56c2bedd4deb4fcf63ad623b8d102ff7cd7a8a9c
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-09-04 17:02:05 -0300

    raw: Importing the font to be used in the project via Google Fonts. Retrieving the colors from the layout and defining them in the application's default theme file. Typing the object containing the theme data to enable access to this data through 'styled-components'. Finally, installing and configuring 'eslint' in the project

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 
 import { Button } from './components/Button'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
       <Button variant="primary">Enviar</Button>
       <Button variant="secondary">Enviar</Button>
       <Button variant="danger">Enviar</Button>
